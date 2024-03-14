227a25824c1fdd604483a8b5be744de0cc0645ef
ewyti <sgdarwin@hotmail.com>
2024-03-13 23:00:45 -0300

    Exercise Pratic 24/03/13

--- convertionTime.js
+++ convertionTime.js
@@ -5,12 +5,24 @@
  * @returns A hora em formato **24 horas**
  */
 function timeConvertion(s) {
-    s.split("");
-
+    s.split(""); //Split para transformar o String em Array.
+    /**
+     * transformando os numeros **String** em numeros **Number** com parseInt e somando eles.
+     */
     let hours = parseInt(s[0] + s[1]);
+    /**
+     * Guardar os minutos, que não precisam ser transformados em Number.
+     */
     let minutes = s[3] + s[4];
+    /**
+     * Guardar os segundos, que não precisam ser transformados em Number.
+     */
     let seconds = s[6] + s[7];
+    /**
+     * Retornando os 2 ultimos valores do Array
+     */
     let amPm = s[s.length - 2];
+    console.log(amPm);
 
     if (hours >= 12 && amPm === "A") {
         hours -= 12;
